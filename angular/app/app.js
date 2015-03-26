b06bdb3c440fc64ce249ee1a5f53f1ba88ae7d78
DannyBoyNYC <daniel.deverell@gmail.com>
2015-03-26 15:00:11 -0400

    prep for class

--- angular/app/app.js
+++
@@ -1,9 +0,0 @@
-	var portfolioApp = angular.module("portfolioSharingApp", ["ngRoute"]);
-
-	portfolioApp.config(function ($routeProvider) {
-		$routeProvider
-			.when("/portfolios",  { controller: "PortfolioListController", templateUrl: "app/partials/portfolio_list_partial.html" })
-		    .when("/portfolio/:portfolio_name",  { controller: "PortfolioViewerController", templateUrl: "app/partials/portfolio_viewer.html" })
-		    .when("/",  { redirectTo: "/portfolios" })
-        	.otherwise({ redirectTo: "/404_page" });
-	});
