ed10834f569424a71cea2da5bb70eded4a587b86
DannyBoyNYC <daniel.deverell@gmail.com>
2015-07-20 11:23:08 -0400

    added multiple controllers and partial

--- angular/app/app.js
+++ angular/app/app.js
@@ -1,5 +1,9 @@
 var portfolioApp = angular.module('portfolioApp', []);
 
+portfolioApp.controller('UserController', function( $scope ) {
+    $scope.user = { name: "Daniel" };
+});
+
 portfolioApp.controller('PortfolioListController', function( $scope ) {
 
 
@@ -16,35 +20,35 @@ portfolioApp.controller('PortfolioListController', function( $scope ) {
 
     $scope.addPortfolio = function( new_portfolio ){
             // if new_portfolio is not defined
-    if (typeof(new_portfolio) === 'undefined') {
+            if (typeof(new_portfolio) === 'undefined') {
         // then add an error message to $scope and exit
         $scope.add_portfolio_error = "The form is not properly filled out";
         return false;
     };
-        if (!new_portfolio.title) 
-        {
-            $scope.add_portfolio_error = "Missing title";
-        }
-        else if (!new_portfolio.date || !is_valid_date(new_portfolio.date))
-        {
-            $scope.add_portfolio_error = "You must provide a date in format yyyy/mm/dd";
-        }
-        else if (!new_portfolio.description)
-        {
-            $scope.add_portfolio_error = "Missing description";
-        }
-        else if (!new_portfolio.name)
-        {
-            $scope.add_portfolio_error = "Missing name - six characters";
-        }
-        else {
-            $scope.portfolios.push( new_portfolio );
-            $scope.adding_portfolio = {};
-            $scope.add_portfolio_error = "";
-        };
+    if (!new_portfolio.title) 
+    {
+        $scope.add_portfolio_error = "Missing title";
+    }
+    else if (!new_portfolio.date || !is_valid_date(new_portfolio.date))
+    {
+        $scope.add_portfolio_error = "You must provide a date in format yyyy/mm/dd";
+    }
+    else if (!new_portfolio.description)
+    {
+        $scope.add_portfolio_error = "Missing description";
+    }
+    else if (!new_portfolio.name)
+    {
+        $scope.add_portfolio_error = "Missing name - six characters";
+    }
+    else {
+        $scope.portfolios.push( new_portfolio );
+        $scope.adding_portfolio = {};
+        $scope.add_portfolio_error = "";
     };
+};
 
-    function is_valid_date (the_date){
+function is_valid_date (the_date){
         //http://eloquentjavascript.net/09_regexp.html
         if (the_date.match(/^[0-9]{4,4}\/[0-9]{2,2}\/[0-9]{2,2}$/)){
             var d = new Date(the_date);
