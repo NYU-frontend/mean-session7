f8091874187db82fb4ea99858572dc8458d8e796
DannyBoyNYC <daniel.deverell@gmail.com>
2015-07-20 16:08:54 -0400

    added portfolio links

--- angular/app/app.js
+++ angular/app/app.js
@@ -2,62 +2,21 @@ var portfolioApp = angular.module('portfolioApp', ['ngRoute']);
 
 portfolioApp.config(function ($routeProvider) {
     $routeProvider
-    .when("/portfolios",  { controller: "PortfolioListController", templateUrl: "app/partials/portfolios.html" })
+    .when("/portfolios",  { 
+        controller: "PortfolioListController", 
+        templateUrl: "app/partials/portfolios.html" })
+
+    .when("/portfolio/:portfolio_name",  { 
+        controller: "PortfolioViewController", 
+        templateUrl: "app/partials/portfolio.html"  })
+
     .when("/",  { redirectTo: "/portfolios" })
     .otherwise({ redirectTo: "/404_page" });
 });
 
 
-portfolioApp.controller('PortfolioListController', function( $scope ) {
 
-    $scope.portfolios = [
-    {name: 'vessel1309', title: 'Call of Booty', date: '2013-09-01', description: 'Arrrgh mateys! This be the finest vessel sailing these seas that yer eyes ever laid sight on. Arrrgh.', imageurl: 'images/drunkenPirate_thumb3.png' },
 
-    {name: 'innocents1404', title: 'The Sack of the Innocents', date: '2014-04-15', description: 'This be a detail of the sacking of the innocents where I played an important role as business analyst and in implementation.', imageurl: 'images/drunkenPirate_thumb2.png' },
 
-    {name: 'firstmate1210', title: 'Pipe and First Mate', date: '2012-10-01', description: 'After a hard day of lootin\' and shootin\' I like to relax with my best mates and tally up the days booty.', imageurl: 'images/drunkenPirate_thumb1.png' }
-    ];
 
-    $scope.new_album = {};
-    $scope.add_portfolio_error = "";
 
-    $scope.addPortfolio = function( new_portfolio ){
-            // if new_portfolio is not defined
-            if (typeof(new_portfolio) === 'undefined') {
-        // then add an error message to $scope and exit
-        $scope.add_portfolio_error = "The form is not properly filled out";
-        return false;
-    };
-    if (!new_portfolio.title) 
-    {
-        $scope.add_portfolio_error = "Missing title";
-    }
-    else if (!new_portfolio.date || !is_valid_date(new_portfolio.date))
-    {
-        $scope.add_portfolio_error = "You must provide a date in format yyyy/mm/dd";
-    }
-    else if (!new_portfolio.description)
-    {
-        $scope.add_portfolio_error = "Missing description";
-    }
-    else if (!new_portfolio.name)
-    {
-        $scope.add_portfolio_error = "Missing name - six characters";
-    }
-    else {
-        $scope.portfolios.push( new_portfolio );
-        $scope.adding_portfolio = {};
-        $scope.add_portfolio_error = "";
-    };
-};
-
-function is_valid_date (the_date){
-        //http://eloquentjavascript.net/09_regexp.html
-        if (the_date.match(/^[0-9]{4,4}\/[0-9]{2,2}\/[0-9]{2,2}$/)){
-            var d = new Date(the_date);
-            return !(isNaN(d.getTime()));
-        } else {
-            return false;
-        };
-    };
-});
